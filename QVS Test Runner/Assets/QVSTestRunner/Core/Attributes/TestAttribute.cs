efc9a85c84ea7bbda8ed252246023fe42d2cbfae
Hrishik Arora <hrishik.arora@gmail.com>
2025-12-08 04:51:03 +0530

    implemented TestClass and Test attributes

---
+++ QVS Test Runner/Assets/QVSTestRunner/Core/Attributes/TestAttribute.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace QVSRunner.Core.Discovery
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class TestAttribute : Attribute
+    {
+
+    }
+}
---
+++ QVS Test Runner/Assets/QVSTestRunner/Core/Attributes/TestClassAttribute.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace QVSRunner.Core.Discovery
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TestClassAttribute : Attribute
+    {
+
+    }
+}
