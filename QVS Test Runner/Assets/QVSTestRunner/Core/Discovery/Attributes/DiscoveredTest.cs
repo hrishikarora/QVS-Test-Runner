73afb7246bfe4772195c87ec77a4c8fbb31e5c9c
Hrishik Arora <hrishik.arora@gmail.com>
2025-12-08 04:52:30 +0530

    added DiscoveredTest data model

---
+++ QVS Test Runner/Assets/QVSTestRunner/Core/Discovery/Attributes/DiscoveredTest.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection;
+
+namespace QVSRunner.Core.Discovery
+{
+    public class DiscoveredTest
+    {
+        public Type ClassType;
+        public MethodInfo Method;
+    }
+}
